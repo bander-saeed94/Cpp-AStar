2e098c99b10146b94271a1151d625c0c14ce1d91
Bander Alshammari <bannadr1@hotmail.com>
2019-07-29 22:03:50 +0300

    remove logging

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -3,20 +3,13 @@
 using std::cout;
 using std::sort;
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
-    cout << "start_x: " << start_x << ", start_y: " << start_y << "\n";
-    cout << "end_x: " << end_x << ", end_y: " << end_y << "\n";
     start_x *= 0.01;
     start_y *= 0.01;
     end_x *= 0.01;
     end_y *= 0.01;
 
-    cout << "start_x: " << start_x << ", start_y: " << start_y << "\n";
-    cout << "end_x: " << end_x << ", end_y: " << end_y << "\n";
-
     start_node = &m_Model.FindClosestNode(start_x, start_y);
     end_node = &m_Model.FindClosestNode(end_x, end_y);
-    cout << "start_node->x: " << start_node->x << ", start_node->y: " << start_node->y << "\n";
-    cout << "end_node->x: " << end_node->x << ", end_node->y: " << end_node->y << "\n";
 }
 vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node * current_node){
     vector<RouteModel::Node> path{};
